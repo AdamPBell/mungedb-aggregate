d73ed9349e6daa7f39fc8f79f38b18bf6708ff3a
Jake Delaney <jdelaney@riverainc.com>
2014-10-30 15:01:26 -0400

    EAGLESIX-2716: test cases re-formatted

--- test/lib/pipeline/expressions/SetIsSubsetExpression.js
+++ test/lib/pipeline/expressions/SetIsSubsetExpression.js
@@ -16,103 +16,100 @@ function errMsg(expr, args, tree, expected, result) {
 
 module.exports = {
 
-		"SetIsSubsetExpression": {
+	"SetIsSubsetExpression": {
 
-				"constructor()": {
+		"constructor()": {
 
-						"should not throw Error when constructing without args": function testConstructor() {
-								assert.doesNotThrow(function() {
-										new SetIsSubsetExpression();
-								});
-						},
+			"should not throw Error when constructing without args": function testConstructor() {
+				assert.doesNotThrow(function() {
+					new SetIsSubsetExpression();
+				});
+			},
 
-						"should throw Error when constructing with args": function testConstructor() {
-								assert.throws(function() {
-										new SetIsSubsetExpression("someArg");
-								});
-						}
+			"should throw Error when constructing with args": function testConstructor() {
+				assert.throws(function() {
+					new SetIsSubsetExpression("someArg");
+				});
+			}
 
-				},
+		},
 
-				"#getOpName()": {
+		"#getOpName()": {
 
-						"should return the correct op name; $setIsSubset": function testOpName() {
-								assert.equal(new SetIsSubsetExpression().getOpName(), "$setIsSubset");
-						}
+			"should return the correct op name; $setIsSubset": function testOpName() {
+				assert.equal(new SetIsSubsetExpression().getOpName(), "$setIsSubset");
+			}
 
-				},
+		},
 
-				"#evaluateInternal()": {
+		"#evaluateInternal()": {
 
-						"Should fail if array1 is not an array": function testArg1() {
-								var array1 = "not an array",
-										array2 = [6, 7, 8, 9];
-								assert.throws(function() {
-										Expression.parseOperand({
-												$setIsSubset: ["$array1", "$array2"]
-										}).evaluateInternal({
-												array1: array1,
-												array2: array2
-										});
-								});
-						},
-
-						"Should fail if array2 is not an array": function testArg2() {
-								var array1 = [1, 2, 3, 4],
-										array2 = "not an array";
-								assert.throws(function() {
-										Expression.parseOperand({
-												$setIsSubset: ["$array1", "$array2"]
-										}).evaluateInternal({
-												array1: array1,
-												array2: array2
-										});
-								});
-						},
-
-						"Should fail if both are not an array": function testArg1andArg2() {
-								var array1 = "not an array",
-										array2 = "not an array";
-								assert.throws(function() {
-										Expression.parseOperand({
-												$setIsSubset: ["$array1", "$array2"]
-										}).evaluateInternal({
-												array1: array1,
-												array2: array2
-										});
-								});
-						},
+			"Should fail if array1 is not an array": function testArg1() {
+				var array1 = "not an array",
+					array2 = [6, 7, 8, 9],
+					input = [array1,array2],
+					idGenerator = new VariablesIdGenerator(),
+					vps = new VariablesParseState(idGenerator),
+					expr = Expression.parseExpression("$setIsSubset", input, vps);
+				assert.throws(function () {
+					expr.evaluate({});
+				});
+			},
 
 
-						"Should pass and return a true": function testBasicAssignment(){
-							var array1 = [2,3],
-								array2 = [1, 2, 3, 4, 5],
-								input = [array1,array2],
-								idGenerator = new VariablesIdGenerator(),
-								vps = new VariablesParseState(idGenerator),
-								expr = Expression.parseExpression("$setIsSubset", input, vps),
-								result = expr.evaluate({}),
-								expected = true,
-								msg = errMsg("$setIsSubset", input, expr.serialize(false), expected, result);
-							assert.equal(result, expected, msg);
-						},
+			"Should fail if array2 is not an array": function testArg2() {
+				var array1 = [1, 2, 3, 4],
+					array2 = "not an array",
+					input = [array1,array2],
+					idGenerator = new VariablesIdGenerator(),
+					vps = new VariablesParseState(idGenerator),
+					expr = Expression.parseExpression("$setIsSubset", input, vps);
+				assert.throws(function () {
+						expr.evaluate({});
+				});
+			},
 
-						"Should pass and return false": function testBasicAssignment() {
-							var array1 = [1, 2, 3, 4, 5],
-								array2 = [7, 8, 9],
-								input = [array1, array2],
-								idGenerator = new VariablesIdGenerator(),
-								vps = new VariablesParseState(idGenerator),
-								expr = Expression.parseExpression("$setIsSubset", input, vps),
-								result = expr.evaluate({}),
-								expected = false,
-								msg = errMsg("$setIsSubset", input, expr.serialize(false), expected, result);
-							assert.equal(result, expected, msg);
-						},
+			"Should fail if both are not an array": function testArg1andArg2() {
+				var array1 = "not an array",
+					array2 = "not an array",
+					input = [array1,array2],
+					idGenerator = new VariablesIdGenerator(),
+					vps = new VariablesParseState(idGenerator),
+					expr = Expression.parseExpression("$setIsSubset", input, vps);
+				assert.throws(function () {
+					expr.evaluate({});
+				});
+			},
 
-				}
+			"Should pass and return a true": function testBasicAssignment(){
+				var array1 = [2,3],
+					array2 = [1, 2, 3, 4, 5],
+					input = [array1,array2],
+					idGenerator = new VariablesIdGenerator(),
+					vps = new VariablesParseState(idGenerator),
+					expr = Expression.parseExpression("$setIsSubset", input, vps),
+					result = expr.evaluate({}),
+					expected = true,
+					msg = errMsg("$setIsSubset", input, expr.serialize(false), expected, result);
+				assert.equal(result, expected, msg);
+			},
+
+			"Should pass and return false": function testBasicAssignment() {
+				var array1 = [1, 2, 3, 4, 5],
+					array2 = [7, 8, 9],
+					input = [array1, array2],
+					idGenerator = new VariablesIdGenerator(),
+					vps = new VariablesParseState(idGenerator),
+					expr = Expression.parseExpression("$setIsSubset", input, vps),
+					result = expr.evaluate({}),
+					expected = false,
+					msg = errMsg("$setIsSubset", input, expr.serialize(false), expected, result);
+				assert.equal(result, expected, msg);
+			},
 
 		}
+
+	}
 
 };
 
