321738ca122b3b3166512bc04df6a7d74dbaaa54
Jake Delaney <jdelaney@riverainc.com>
2014-10-30 15:37:59 -0400

    EAGLESIX-2715: test cases work

--- test/lib/pipeline/expressions/SetUnionExpression.js
+++ test/lib/pipeline/expressions/SetUnionExpression.js
@@ -1,118 +1,130 @@
 "use strict";
 var assert = require("assert"),
-		SetUnionExpression = require("../../../../lib/pipeline/expressions/SetUnionExpression"),
-		Expression = require("../../../../lib/pipeline/expressions/Expression");
+	VariablesIdGenerator = require("../../../../lib/pipeline/expressions/VariablesIdGenerator"),
+	VariablesParseState = require("../../../../lib/pipeline/expressions/VariablesParseState"),
+	SetUnionExpression = require("../../../../lib/pipeline/expressions/SetUnionExpression"),
+	Expression = require("../../../../lib/pipeline/expressions/Expression");
 
+function errMsg(expr, args, tree, expected, result) {
+	return "for expression " + expr +
+		" with argument " + args +
+		" full tree: " + JSON.stringify(tree) +
+		" expected: " + expected +
+		" result: " + result;
+}
 
 module.exports = {
 
-		"SetUnionExpression": {
+	"SetUnionExpression": {
 
-				"constructor()": {
+		"constructor()": {
 
-						"should throw Error when constructing without args": function testConstructor() {
-								assert.throws(function() {
-										new SetUnionExpression();
-								});
-						}
+			"should not throw Error when constructing without args": function testConstructor() {
+				assert.doesNotThrow(function() {
+						new SetUnionExpression();
+				});
+			},
 
-				},
+			"should throw Error when constructing with args": function testConstructor() {
+				assert.throws(function() {
+						new SetUnionExpression("someArg");
+				});
+			}
 
-				"#getOpName()": {
+		},
 
-						"should return the correct op name; $setUnion": function testOpName() {
-								assert.equal(new SetUnionExpression([1, 2, 3], [4, 5, 6]).getOpName(), "$setUnion");
-						}
+		"#getOpName()": {
 
-				},
+			"should return the correct op name; $setUnion": function testOpName() {
+				assert.equal(new SetUnionExpression().getOpName(), "$setUnion");
+			}
 
-				"#evaluateInternal()": {
+		},
 
-						"Should fail if array1 is not an array": function testArg1() {
-								var array1 = "not an array",
-										array2 = [6, 7, 8, 9];
-								assert.throws(function() {
-										Expression.parseOperand({
-												$setUnion: ["$array1", "$array2"]
-										}).evaluateInternal({
-												array1: array1,
-												array2: array2
-										});
-								});
-						},
+		"#evaluateInternal()": {
 
-						"Should fail if array2 is not an array": function testArg2() {
-								var array1 = [1, 2, 3, 4],
-										array2 = "not an array";
-								assert.throws(function() {
-										Expression.parseOperand({
-												$setUnion: ["$array1", "$array2"]
-										}).evaluateInternal({
-												array1: array1,
-												array2: array2
-										});
-								});
-						},
+			"Should fail if array1 is not an array": function testArg1() {
+				var array1 = "not an array",
+					array2 = [6, 7, 8, 9],
+					input = [array1,array2],
+					idGenerator = new VariablesIdGenerator(),
+					vps = new VariablesParseState(idGenerator),
+					expr = Expression.parseExpression("$setUnion", input, vps);
+				assert.throws(function () {
+					expr.evaluate({});
+				});
+			},
 
-						"Should fail if both are not an array": function testArg1andArg2() {
-								var array1 = "not an array",
-										array2 = "not an array";
-								assert.throws(function() {
-										Expression.parseOperand({
-												$setUnion: ["$array1", "$array2"]
-										}).evaluateInternal({
-												array1: array1,
-												array2: array2
-										});
-								});
-						},
+			"Should fail if array2 is not an array": function testArg2() {
+				var array1 = [1, 2, 3, 4],
+					array2 = "not an array",
+					input = [array1,array2],
+					idGenerator = new VariablesIdGenerator(),
+					vps = new VariablesParseState(idGenerator),
+					expr = Expression.parseExpression("$setUnion", input, vps);
+				assert.throws(function () {
+					expr.evaluate({});
+				});
+			},
 
-						"Should pass and return a unioned set1": function testBasicAssignment() {
-								var array1 = {
-										"a": "3",
-										"c": "4"
-								},
-										array2 = {
-												"a": "3",
-												"b": "3"
-										};
-								assert.strictEqual(Expression.parseOperand({
-										$setUnion: ["$array1", "$array2"]
-								}).evaluateInternal({
-										array1: array1,
-										array2: array2
-								}), {
-										"a": "3",
-										"c": "4",
-										"b": "3"
-								});
-						},
+			"Should fail if both are not an array": function testArg1andArg2() {
+				var array1 = "not an array",
+					array2 = "not an array",
+					input = [array1,array2],
+					idGenerator = new VariablesIdGenerator(),
+					vps = new VariablesParseState(idGenerator),
+					expr = Expression.parseExpression("$setUnion", input, vps);
+				assert.throws(function () {
+					expr.evaluate({});
+				});
+			},
 
-						"Should pass and return a unioned set": function testBasicAssignment() {
-								var array1 = [1, 2, 3, 4, 5],
-										array2 = [2, 3, 6, 7, 8];
-								assert.strictEqual(Expression.parseOperand({
-										$setUnion: ["$array1", "$array2"]
-								}).evaluateInternal({
-										array1: array1,
-										array2: array2
-								}), [1, 2, 3, 4, 5, 6, 7, 8]);
-						},
+			"Should pass and return [1, 2, 3, 4, 5, 8, 9]": function testBasicAssignment(){
+				var array1 = [1, 2, 3, 9, 8],
+					array2 = [1, 2, 3, 4, 5],
+					input = [array1,array2],
+					idGenerator = new VariablesIdGenerator(),
+					vps = new VariablesParseState(idGenerator),
+					expr = Expression.parseExpression("$setUnion", input, vps),
+					result = expr.evaluate({}),
+					expected = [1, 2, 3, 4, 5, 8, 9],
+					msg = errMsg("$setUnion", input, expr.serialize(false), expected, result);
+				assert.deepEqual(result, expected, msg);
+			},
 
-						"Should pass and return unioned set": function testBasicAssignment() {
-								var array1 = [1, 2, 3, 4, 5],
-										array2 = [7, 8, 9];
-								assert.strictEqual(Expression.parseOperand({
-										$setUnion: ["$array1", "$array2"]
-								}).evaluateInternal({
-										array1: array1,
-										array2: array2
-								}), [1, 2, 3, 4, 5, 7, 8, 9]);
-						},
+			"Should pass and return [1, 2, 3, 4, 5, 7]": function testBasicAssignment() {
+				var array1 = [2, 4],
+					array2 = [1, 2, 3, 4, 5],
+					array3 = [7, 2, 1],
+					array4 = [],
+					input = [array1,array2,array3, array4],
+					idGenerator = new VariablesIdGenerator(),
+					vps = new VariablesParseState(idGenerator),
+					expr = Expression.parseExpression("$setUnion", input, vps),
+					result = expr.evaluate({}),
+					expected = [1, 2, 3, 4, 5, 7],
+					msg = errMsg("$setUnion", input, expr.serialize(false), expected, result);
+				assert.deepEqual(result, expected, msg);
+			},
 
-				}
+			"Should pass and return [1, 2, 7]": function testBasicAssignment() {
+				var array1 = [],
+					array2 = [],
+					array3 = [7, 2, 1],
+					array4 = [],
+					input = [array1,array2,array3, array4],
+					idGenerator = new VariablesIdGenerator(),
+					vps = new VariablesParseState(idGenerator),
+					expr = Expression.parseExpression("$setUnion", input, vps),
+					result = expr.evaluate({}),
+					expected = [1, 2, 7],
+					msg = errMsg("$setUnion", input, expr.serialize(false), expected, result);
+				assert.deepEqual(result, expected, msg);
+			}
 
 		}
+
+	}
 
 };
 
