856b2652632a604ef7de9ad15ca0e6c8449c2202
Austin Meagher <ameagher@riverainc.com>
2014-10-31 15:53:10 -0400

    EAGLESIX-2993 added more test cases

--- test/lib/pipeline/matcher/ComparisonMatchExpression.js
+++ test/lib/pipeline/matcher/ComparisonMatchExpression.js
@@ -1,5 +1,8 @@
 "use strict";
 var assert = require("assert"),
+	bson = require("bson"),
+	MinKey = bson.BSONPure.MinKey,
+	MaxKey = bson.BSONPure.MaxKey,
 	MatchDetails = require("../../../../lib/pipeline/matcher/MatchDetails"),
 	ComparisonMatchExpression = require("../../../../lib/pipeline/matcher/ComparisonMatchExpression");
 
@@ -9,13 +12,11 @@ if(!module.parent)return(require.cache[__filename]=null,(new(require("mocha"))({
 exports.ComparisonMatchExpression = {
 
 	"should properly initialize with an empty path and a number": function () {
-		var e = new ComparisonMatchExpression();
-		e._matchType = 'LT';
+		var e = new ComparisonMatchExpression('LT');
 		assert.strictEqual(e.init('',5).code,'OK');
 	},
 	"should not initialize when given an invalid operand": function() {
-		var e = new ComparisonMatchExpression();
-		e._matchType = '';
+		var e = new ComparisonMatchExpression('');
 		assert.strictEqual(e.init('',5).code, 'BAD_VALUE');
 	},
 	"should not initialize when given an undefined rhs": function() {
@@ -27,8 +28,7 @@ exports.ComparisonMatchExpression = {
 		assert.strictEqual(e.init('',{}).code,'BAD_VALUE');
 	},
 	"should match numbers with GTE": function () {
-		var e = new ComparisonMatchExpression();
-		e._matchType = 'GTE';
+		var e = new ComparisonMatchExpression('GTE');
 		assert.strictEqual(e.init('',5).code,'OK');
 		assert.ok(e.matchesSingleElement(6),'6 ≥ 5');
 		assert.ok(e.matchesSingleElement(5),'5 ≥ 5');
@@ -36,14 +36,12 @@ exports.ComparisonMatchExpression = {
 		assert.ok(!e.matchesSingleElement('foo'),"'foo' !≥ 5");
 	},
 	"should match with simple paths and GTE": function() {
-		var e = new ComparisonMatchExpression();
-		e._matchType = 'GTE';
+		var e = new ComparisonMatchExpression('GTE');
 		assert.strictEqual(e.init('a',5).code,'OK');
 		assert.ok(e.matches({'a':6}));
 	},
 	"should match array values with GTE": function () {
-		var e = new ComparisonMatchExpression();
-		e._matchType = 'GTE';
+		var e = new ComparisonMatchExpression('GTE');
 		assert.strictEqual(e.init('a',5).code,'OK');
 		assert.ok(e.matches({'a':[6,10]}),'[6,10] ≥ 5');
 		assert.ok(e.matches({'a':[4,5.5]}),'[4,5.5] ≥ 5');
@@ -51,8 +49,7 @@ exports.ComparisonMatchExpression = {
 		assert.ok(e.matches({'a':[1,10]}),'[1,10] ≥ 5');
 	},
 	"should match entire arrays with GTE": function() {
-		var e = new ComparisonMatchExpression();
-		e._matchType = 'GTE';
+		var e = new ComparisonMatchExpression('GTE');
 		assert.strictEqual(e.init('a',[5]).code,'OK');
 		assert.ok(!e.matches({'a':[4]}),'[4] !≥ [5]');
 		assert.ok(e.matches({'a':[5]}),'[5] !≥ [5]');
@@ -63,7 +60,7 @@ exports.ComparisonMatchExpression = {
 		assert.ok(e.matches({'a':[[6]]}),'[[6]] ≥ [5]');
 	},
 	"should match null with GTE": function() {
-		var e = new ComparisonMatchExpression();
+		var e = new ComparisonMatchExpression('GTE');
 		e._matchType = 'GTE';
 		assert.strictEqual(e.init('a',null).code,'OK');
 		assert.ok(e.matches({}),'{} ≥ null');
@@ -72,8 +69,7 @@ exports.ComparisonMatchExpression = {
 		assert.ok(e.matches({'b':null}),'non-existent field ≥ null');
 	},
 	"should match null in dotted paths with GTE": function() {
-		var e = new ComparisonMatchExpression();
-		e._matchType = 'GTE';
+		var e = new ComparisonMatchExpression('GTE');
 		assert.strictEqual(e.init('a.b',null).code,'OK');
 		assert.ok(e.matches({}),'{} ≥ null');
 		assert.ok(e.matches({'a':null}),'{a:null} ≥ {a.b:null}');
@@ -84,10 +80,23 @@ exports.ComparisonMatchExpression = {
 		assert.ok(!e.matches({'a':[4]}),'{a:[4]} !≥ {a.b:null}');
 		assert.ok(!e.matches({'a':[{'b':4}]}),'{a:[{b:4}]} !≥ {a.b:null}');
 	},
-	"should match some weird stuff": function() {
-		var e = new ComparisonMatchExpression(),
+	"something something minkeys": function() {
+		var e = new ComparisonMatchExpression('GTE');
+		assert.strictEqual(e.init('a',new MinKey()).code,'OK');
+		assert.ok(e.matches({'a':new MinKey()}),'minKey ≥ minKey');
+		assert.ok(e.matches({'a':new MaxKey()}),'maxKey ≥ minKey');
+		assert.ok(e.matches({'a':4}),'4 ≥ minKey');
+	},
+	"something something maxkeys": function() {
+		var e = new ComparisonMatchExpression('GTE');
+		assert.strictEqual(e.init('a',new MaxKey()).code,'OK');
+		assert.ok(e.matches({'a':new MaxKey()}),'maxKey ≥ maxKey');
+		assert.ok(!e.matches({'a':new MinKey()}),'minKey !≥ maxKey');
+		assert.ok(!e.matches({'a':4},null),'4 !≥ maxKey');
+	},
+	"should properly set match keys": function() {
+		var e = new ComparisonMatchExpression('GTE'),
 			d = new MatchDetails();
-		e._matchType = 'GTE';
 		d.requestElemMatchKey();
 		assert.strictEqual(e.init('a',5).code,'OK');
 		assert.ok(!e.matchesJSON({'a':4},d),'4 !≥ 5');
