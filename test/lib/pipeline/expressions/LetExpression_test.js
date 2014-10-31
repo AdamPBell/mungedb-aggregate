c99711f03a4183b4a00cb8ca4208d42433886cc2
Tony Ennis <tennis@riverainc.com>
2014-10-31 11:11:40 -0400

    EAGLESIX-2695 Added the check for the number of arguments. Added rudimentary tests.

--- test/lib/pipeline/expressions/LetExpression_test.js
+++ test/lib/pipeline/expressions/LetExpression_test.js
@@ -1,3 +1,32 @@
-/**
- * Created by tennis on 10/31/14.
- */
+"use strict";
+var assert = require("assert"),
+	LetExpression = require("../../../../lib/pipeline/expressions/LetExpression"),
+	Expression = require("../../../../lib/pipeline/expressions/Expression");
+
+
+module.exports = {
+
+	"LetExpression": {
+
+		"constructor()": {
+
+			"should throw an Error when constructing without args": function () {
+				assert.throws(function () {
+					new LetExpression();
+				});
+			},
+			"should throw Error when constructing with one arg": function () {
+				assert.throws(function () {
+					new LetExpression(1);
+				});
+			},
+			"should not throw when constructing with two args": function () {
+				assert.doesNotThrow(function () {
+					new LetExpression(1, 2);
+				});
+			}
+		}
+	}
+};
+
+if (!module.parent)(new(require("mocha"))()).ui("exports").reporter("spec").addFile(__filename).run(process.exit);
