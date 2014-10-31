095c97b5b5da76243bdf39d3cb50ad43dbc652ed
Kyle P Davis <KDavis@RiveraInc.com>
2014-10-31 11:19:14 -0400

    EAGLESIX-2651: Not: misc formatting from 2.6.5 port

--- test/lib/pipeline/expressions/NotExpression_test.js
+++ test/lib/pipeline/expressions/NotExpression_test.js
@@ -3,48 +3,45 @@ var assert = require("assert"),
 	NotExpression = require("../../../../lib/pipeline/expressions/NotExpression"),
 	Expression = require("../../../../lib/pipeline/expressions/Expression");
 
+// Mocha one-liner to make these tests self-hosted
+if(!module.parent)return(require.cache[__filename]=null,(new(require("mocha"))({ui:"exports",reporter:"spec",grep:process.env.TEST_GREP})).addFile(__filename).run(process.exit));
 
-module.exports = {
+exports.NotExpression = {
 
-	"NotExpression": {
+	"constructor()": {
 
-		"constructor()": {
-
-			"should not throw Error when constructing without args": function testConstructor(){
-				assert.doesNotThrow(function(){
-					new NotExpression();
-				});
-			},
-			"should throw when constructing with args": function testConstructor(){
-				assert.throws(function(){
-					new NotExpression(1);
-				});
-			}
-
+		"should not throw Error when constructing without args": function() {
+			assert.doesNotThrow(function(){
+				new NotExpression();
+			});
 		},
 
-		"#getOpName()": {
-
-			"should return the correct op name; $not": function testOpName(){
-				assert.equal(new NotExpression().getOpName(), "$not");
-			}
-
+		"should throw when constructing with args": function() {
+			assert.throws(function(){
+				new NotExpression(1);
+			});
 		},
 
-		"#evaluateInternal()": {
+	},
 
-			"should return false for a true input; false for true": function(){
-				assert.strictEqual(Expression.parseOperand({$not:true}, {}).evaluateInternal({}), false);
-			},
+	"#getOpName()": {
 
-			"should return true for a false input; true for false": function(){
-				assert.strictEqual(Expression.parseOperand({$not:false}, {}).evaluateInternal({}), true);
-			}
+		"should return the correct op name; $not": function() {
+			assert.equal(new NotExpression().getOpName(), "$not");
+		},
 
-		}
+	},
 
-	}
+	"#evaluate()": {
+
+		"should return false for a true input; false for true": function() {
+			assert.strictEqual(Expression.parseOperand({$not:true}, {}).evaluateInternal({}), false);
+		},
+
+		"should return true for a false input; true for false": function() {
+			assert.strictEqual(Expression.parseOperand({$not:false}, {}).evaluateInternal({}), true);
+		},
+
+	},
 
 };
-
-if (!module.parent)(new(require("mocha"))()).ui("exports").reporter("spec").addFile(__filename).run(process.exit);
