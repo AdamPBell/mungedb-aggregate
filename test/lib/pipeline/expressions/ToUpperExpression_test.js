b84917f5307e3851e600d046103643a947342e06
Tony Ennis <tennis@riverainc.com>
2014-10-31 09:08:32 -0400

    EAGLESIX-2723 minor changes to ExpressionToUpper, test cases filled out.

--- test/lib/pipeline/expressions/ToUpperExpression_test.js
+++ test/lib/pipeline/expressions/ToUpperExpression_test.js
@@ -1,64 +1,66 @@
 "use strict";
 var assert = require("assert"),
-		ToUpperExpression = require("../../../../lib/pipeline/expressions/ToUpperExpression"),
-		Expression = require("../../../../lib/pipeline/expressions/Expression");
+	ToUpperExpression = require("../../../../lib/pipeline/expressions/ToUpperExpression"),
+	VariablesParseState = require("../../../../lib/pipeline/expressions/VariablesParseState"),
+	VariablesIdGenerator = require("../../../../lib/pipeline/expressions/VariablesIdGenerator"),
+	Expression = require("../../../../lib/pipeline/expressions/Expression");
 
 
 module.exports = {
 
-		"ToUpperExpression": {
+	"ToUpperExpression": {
+		
+		beforeEach: function(){
+			this.vps = new VariablesParseState(new VariablesIdGenerator());
+		},
 
-				"constructor()": {
+		"constructor()": {
 
-						"should not throw Error when constructing without args": function testConstructor() {
-								assert.doesNotThrow(function() {
-										new ToUpperExpression();
-								});
-						},
+			"should not throw Error when constructing without args": function testConstructor() {
+				assert.doesNotThrow(function () {
+					new ToUpperExpression();
+				});
+			},
 
-					"should throw Error when constructing with args": function testConstructor(){
-						assert.throws(function(){
-							new ToUpperExpression(1);
-						});
-					}
+			"should throw Error when constructing with args": function testConstructor() {
+				assert.throws(function () {
+					new ToUpperExpression(1);
+				});
+			}
 
-				},
+		},
 
-				"#getOpName()": {
+		"#getOpName()": {
 
-						"should return the correct op name; $toUpper": function testOpName() {
-								assert.equal(new ToUpperExpression().getOpName(), "$toUpper");
-						}
+			"should return the correct op name; $toUpper": function testOpName() {
+				assert.equal(new ToUpperExpression().getOpName(), "$toUpper");
+			}
 
-				},
+		},
 
-				"#evaluateInternal()": {
-
-						"should return the uppercase version of the string if there is a null character in the middle of the string": function testStuff() {
-								assert.strictEqual(Expression.parseOperand({
-										$toUpper: "$a"
-								}).evaluateInternal({
-										a: "a\0B"
-								}), "A\0B");
-						},
-						"should return the uppercase version of the string if there is a null character at the beginning of the string": function testStuff() {
-								assert.strictEqual(Expression.parseOperand({
-										$toUpper: "$a"
-								}).evaluateInternal({
-										a: "\0aB"
-								}), "\0AB");
-						},
-						"should return the uppercase version of the string if there is a null character at the end of the string": function testStuff() {
-								assert.strictEqual(Expression.parseOperand({
-										$toUpper: "$a"
-								}).evaluateInternal({
-										a: "aB\0"
-								}), "AB\0");
-						}
-				}
-
+		"#evaluateInternal()": {
+			"should uppercase a string": function(){
+				assert.strictEqual(Expression.parseOperand({$toUpper: "$a"}, this.vps).evaluate({a: "now is the time"}), "NOW IS THE TIME");
+			},
+			"should not change symbols": function(){
+				var symbs = "!@#$%^&*()_+{}[]:\";'<>?/.,;";
+				assert.strictEqual(Expression.parseOperand({$toUpper: "$a"}, this.vps).evaluate({a: symbs}), symbs);
+			},
+			"should not change uppercase": function(){
+				var symbs = "NOW IS THE TIME FOR ALL GOOD MEN TO COME FROM THE AID OF THEIR COMPUTERS";
+				assert.strictEqual(Expression.parseOperand({$toUpper: "$a"}, this.vps).evaluate({a: symbs}), symbs);
+			},
+			"should return the uppercase version of the string if there is a null character in the middle of the string": function testStuff() {
+				assert.strictEqual(Expression.parseOperand({$toUpper: "$a"}, this.vps).evaluate({a: "a\0B"}), "A\0B");
+			},
+			"should return the uppercase version of the string if there is a null character at the beginning of the string": function testStuff() {
+				assert.strictEqual(Expression.parseOperand({$toUpper: "$a"}, this.vps).evaluate({a: "\0aB"}), "\0AB");
+			},
+			"should return the uppercase version of the string if there is a null character at the end of the string": function testStuff() {
+				assert.strictEqual(Expression.parseOperand({$toUpper: "$a"}, this.vps).evaluate({a: "aB\0"}), "AB\0");
+			}
 		}
-
+	}
 };
 
-if (!module.parent)(new(require("mocha"))()).ui("exports").reporter("spec").addFile(__filename).run(process.exit);
+if (!module.parent)(new (require("mocha"))()).ui("exports").reporter("spec").addFile(__filename).run(process.exit);
