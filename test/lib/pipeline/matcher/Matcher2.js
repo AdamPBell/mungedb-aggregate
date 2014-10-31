cf5ec1500e2e790b22c68f90bf9ba444b85d8fcd
Jason Walton <JWalton@riverainc.com>
2014-10-31 11:12:56 -0400

    EAGLESIX-3015: Working on matcher test cases

--- test/lib/pipeline/matcher/Matcher2.js
+++ test/lib/pipeline/matcher/Matcher2.js
@@ -6,27 +6,39 @@ var assert = require("assert"),
 module.exports = {
 	"Matcher2": {
 		"Constructor": function() {
-			//var m = new Matcher2();
+			var m = new Matcher2({"a":1});
 		},
 
 		"Basic": function() {
-			var json = {"a":"b"},
-				m = new Matcher2(json);
-			assert(m.matches(json));
+			var query = {"a":"b"},
+				m = new Matcher2(query);
+			assert(m.matches(query));
 		},
 
 		"DoubleEqual": function() {
-			var json = {"a":5},
-				m = new Matcher2(json);
-			assert(m.matches(json));
+			var query = {"a":5},
+				m = new Matcher2(query);
+			assert(m.matches(query));
 		},
 
-		"MixedNumericEqual": function() {
-			var query = {"a":5};
+		"MixedNumericEqual": function() {	//not sure if we need this.  Same as DoubleEqual in munge
+			var query = {"a":5},
+				m = new Matcher2(query);
+			assert(m.matches(query));
 		},
 
 		"MixedNumericGt": function() {
-			var query =  {"a":{"$gt":4}};
+			var query = {"a":{"$gt":4}},
+				m = new Matcher2(query);
+			assert.ok(m.matches({"a":5}));
+		},
+
+		"MixedNumericIN": function() {
+			var query = {"a":{"$in":[4,6]}},
+				m = new Matcher2(query);
+			assert.ok(m.matches({"a":4.0}));
+			assert.ok(m.matches({"a":5.0}));
+			assert.ok(m.matches({"a":4}));
 		}
 	}
 }
