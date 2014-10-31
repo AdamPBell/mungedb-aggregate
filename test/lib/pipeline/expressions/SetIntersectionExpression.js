7a26e023e936fcd209ea6f1a5fb0f56b25a74037
Tony Ennis <tennis@riverainc.com>
2014-10-31 12:59:52 -0400

    EAGLESIX-2715 Reviewed test cases, filled out some missing ones, refectored common code.

--- test/lib/pipeline/expressions/SetIntersectionExpression.js
+++ test/lib/pipeline/expressions/SetIntersectionExpression.js
@@ -44,66 +44,58 @@ module.exports = {
 
 		"#evaluateInternal()": {
 
+			beforeEach: function(){
+				this.vps = new VariablesParseState(new VariablesIdGenerator());
+				this.checkNotArray = function(array1, array2) {
+					var input = [array1,array2],
+						expr = Expression.parseExpression("$setIntersection", input, this.vps);
+					assert.throws(function() {
+						expr.evaluate({});
+					});
+				};
+				this.checkIntersection = function(array1, array2, expected) {
+					var input = [array1,array2],
+						expr = Expression.parseExpression("$setIntersection", input, this.vps),
+						result = expr.evaluate({}),
+						msg = errMsg("$setIntersection", input, expr.serialize(false), expected, result);
+					assert.deepEqual(result, expected, msg);
+				};
+				this.checkIntersectionBothWays = function(array1, array2, expected) {
+					this.checkIntersection(array1, array2, expected);
+					this.checkIntersection(array2, array1, expected);
+				};
+			},
+
 			"Should fail if array1 is not an array": function testArg1() {
-				var array1 = "not an array",
-					array2 = [6, 7, 8, 9],
-					input = [array1,array2],
-					idGenerator = new VariablesIdGenerator(),
-					vps = new VariablesParseState(idGenerator),
-					expr = Expression.parseExpression("$setIntersection", input, vps);
-				assert.throws(function() {
-					expr.evaluate({});
-				});
+				this.checkNotArray("not an array", [6, 7, 8, 9]);
 			},
 
 			"Should fail if array2 is not an array": function testArg2() {
-				var array1 = [1, 2, 3, 4],
-					array2 = "not an array",
-					input = [array1,array2],
-					idGenerator = new VariablesIdGenerator(),
-					vps = new VariablesParseState(idGenerator),
-					expr = Expression.parseExpression("$setIntersection", input, vps);
-				assert.throws(function() {
-					expr.evaluate({});
-				});
+				this.checkNotArray([1, 2, 3, 4], "not an array");
 			},
 
 			"Should fail if both are not an array": function testArg1andArg2() {
-				var array1 = "not an array",
-					array2 = "not an array",
-					input = [array1,array2],
-					idGenerator = new VariablesIdGenerator(),
-					vps = new VariablesParseState(idGenerator),
-					expr = Expression.parseExpression("$setIntersection", input, vps);
-				assert.throws(function() {
-					expr.evaluate({});
-				});
+				this.checkNotArray("not an array","not an array");
 			},
 
 			"Should pass and return [2, 3]": function testBasicAssignment(){
-				var array1 = [2,3],
-					array2 = [1, 2, 3, 4, 5],
-					input = [array1,array2],
-					idGenerator = new VariablesIdGenerator(),
-					vps = new VariablesParseState(idGenerator),
-					expr = Expression.parseExpression("$setIntersection", input, vps),
-					result = expr.evaluate({}),
-					expected = [2, 3],
-					msg = errMsg("$setIntersection", input, expr.serialize(false), expected, result);
-				assert.deepEqual(result, expected, msg);
+				this.checkIntersectionBothWays([2,3], [1, 2, 3, 4, 5], [2, 3]);
 			},
 
-			"Should pass and return []": function testBasicAssignment() {
-				var array1 = [1, 2, 3, 4, 5],
-					array2 = [7, 8, 9],
-					input = [array1, array2],
-					idGenerator = new VariablesIdGenerator(),
-					vps = new VariablesParseState(idGenerator),
-					expr = Expression.parseExpression("$setIntersection", input, vps),
-					result = expr.evaluate({}),
-					expected = [],
-					msg = errMsg("$setIntersection", input, expr.serialize(false), expected, result);
-				assert.deepEqual(result, expected, msg);
+			"Should pass and return []": function() {
+				this.checkIntersectionBothWays([1, 2, 3, 4, 5], [7, 8, 9], []);
+			},
+
+			"Should handle when a set is empty": function() {
+				this.checkIntersectionBothWays([], [7, 8, 9], []);
+			},
+
+			"Should work when we touch the ends": function() {
+				this.checkIntersectionBothWays([7, 9], [7, 8, 9], [7, 9]);
+			},
+
+			"Should work when both sets are empty": function() {
+				this.checkIntersectionBothWays([], [], []);
 			}
 
 		}
