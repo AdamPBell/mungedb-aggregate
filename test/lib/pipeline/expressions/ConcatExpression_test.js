2604933da061d914cc10f14fae6b58712d107d03
Kyle P Davis <KDavis@RiveraInc.com>
2014-11-04 00:48:22 -0500

    EAGLESIX-2651: Concat: better sync w/ 2.6.5 code (mainly reordering and code formatting), reformat tests a little bit

--- test/lib/pipeline/expressions/ConcatExpression_test.js
+++ test/lib/pipeline/expressions/ConcatExpression_test.js
@@ -5,78 +5,87 @@ var assert = require("assert"),
 	VariablesIdGenerator = require("../../../../lib/pipeline/expressions/VariablesIdGenerator"),
 	Expression = require("../../../../lib/pipeline/expressions/Expression");
 
+// Mocha one-liner to make these tests self-hosted
+if(!module.parent)return(require.cache[__filename]=null,(new(require("mocha"))({ui:"exports",reporter:"spec",grep:process.env.TEST_GREP})).addFile(__filename).run(process.exit));
 
-module.exports = {
+exports.ConcatExpression = {
 
-	"ConcatExpression": {
-		beforeEach: function(){
-			this.vps = new VariablesParseState(new VariablesIdGenerator());
+	beforeEach: function() {
+		this.vps = new VariablesParseState(new VariablesIdGenerator());
+	},
+
+	"constructor()": {
+
+		"should not throw Error when constructing without args": function() {
+			assert.doesNotThrow(function() {
+				new ConcatExpression();
+			});
 		},
 
-		"constructor()": {
-
-			"should not throw Error when constructing without args": function testConstructor(){
-				assert.doesNotThrow(function(){
-					new ConcatExpression();
-				});
-			},
-			"should throw Error when constructing with args": function testConstructor(){
-				assert.throws(function(){
-					new ConcatExpression("should die");
-				});
-			}
+		"should throw Error when constructing with args": function() {
+			assert.throws(function() {
+				new ConcatExpression("should die");
+			});
 		},
 
-		"#getOpName()": {
+	},
 
-			"should return the correct op name; $concat": function testOpName(){
-				assert.equal(new ConcatExpression().getOpName(), "$concat");
-			}
+	"#getOpName()": {
 
+		"should return the correct op name; $concat": function() {
+			assert.equal(new ConcatExpression().getOpName(), "$concat");
 		},
 
-		"#evaluate()": {
+	},
 
-			"should return empty string if no operands were given; {$concat:[]}": function testEmpty(){
-				assert.equal(Expression.parseOperand({$concat:[]}, this.vps).evaluate(), "");
-			},
+	"#evaluate()": {
 
-			"should return mystring if operands are my string; {$concat:[my, string]}": function testConcat(){
-				assert.equal(Expression.parseOperand({$concat:["my", "string"]}, this.vps).evaluate(), "mystring");
-			},
+		"should return empty string if no operands were given; {$concat:[]}": function() {
+			var expr = Expression.parseOperand({$concat:[]}, this.vps);
+			assert.equal(expr.evaluate(), "");
+		},
 
-			"should return mystring if operands are my and $a; {$concat:[my,$a]}": function testFieldPath(){
-				assert.equal(Expression.parseOperand({$concat:["my","$a"]}, this.vps).evaluate({a:"string"}), "mystring");
-			},
-			"should return null if an operand evaluates to null; {$concat:[my,$a]}": function testNull(){
-				var a = Expression.parseOperand({$concat:["my","$a"]}, this.vps);
-				var b = a.evaluate({a:null});
-				assert.equal(b, null);
-			},
-			"should return null if an operand evaluates to undefined; {$concat:[my,$a]}": function testNull(){
-				var a = Expression.parseOperand({$concat:["my","$a"]}, this.vps);
-				var b = a.evaluate({a:undefined});
-				assert.equal(b, null);
-			},
-			"should throw if an operand is a number": function testNull(){
-				assert.throws(function(){
-					Expression.parseOperand({$concat:["my","$a"]}, this.vps).evaluate({a:100});
-				});
-			},
-			"should throw if an operand is a date": function testNull(){
-				assert.throws(function(){
-					Expression.parseOperand({$concat:["my","$a"]}, this.vps).evaluate({a:new Date()});
-				});
-			},
-			"should throw if an operand is a boolean": function testNull(){
-				assert.throws(function(){
-					Expression.parseOperand({$concat:["my","$a"]}, this.vps).evaluate({a:true});
-				});
-			}
-		}
+		"should return mystring if operands are my string; {$concat:[my, string]}": function() {
+			var expr = Expression.parseOperand({$concat:["my", "string"]}, this.vps);
+			assert.equal(expr.evaluate(), "mystring");
+		},
 
-	}
+		"should return mystring if operands are my and $a; {$concat:[my,$a]}": function() {
+			var expr = Expression.parseOperand({$concat:["my","$a"]}, this.vps);
+			assert.equal(expr.evaluate({a:"string"}), "mystring");
+		},
+
+		"should return null if an operand evaluates to null; {$concat:[my,$a]}": function() {
+			var expr = Expression.parseOperand({$concat:["my","$a"]}, this.vps);
+			assert.equal(expr.evaluate({a:null}), null);
+		},
+
+		"should return null if an operand evaluates to undefined; {$concat:[my,$a]}": function() {
+			var expr = Expression.parseOperand({$concat:["my","$a"]}, this.vps);
+			assert.equal(expr.evaluate({a:undefined}), null);
+		},
+
+		"should throw if an operand is a number": function() {
+			var expr = Expression.parseOperand({$concat:["my","$a"]}, this.vps);
+			assert.throws(function() {
+				expr.evaluate({a:100});
+			});
+		},
+
+		"should throw if an operand is a date": function() {
+			var expr = Expression.parseOperand({$concat:["my","$a"]}, this.vps);
+			assert.throws(function() {
+				expr.evaluate({a:new Date()});
+			});
+		},
+
+		"should throw if an operand is a boolean": function() {
+			var expr = Expression.parseOperand({$concat:["my","$a"]}, this.vps)
+			assert.throws(function() {
+				expr.evaluate({a:true});
+			});
+		},
+
+	},
 
 };
-
-if (!module.parent)(new(require("mocha"))()).ui("exports").reporter("spec").addFile(__filename).run(process.exit);
